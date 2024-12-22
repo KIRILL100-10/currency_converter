14ab30656af95066f493a797a8d61d9412034b69
KIRILL100_10 <Kirillnadtocaev@gmail.com>
2024-12-22 15:25:52 +0300

    Update currency_converter

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def currency_converter(base_currency, target_currency, amount):
         response = requests.get(url)
         response_json = response.json()
         if response.status_code == 200 and response_json['result'] == 'success':
-            return f'{amount} {base_currency} = {response_json['conversion_result']:.2f} {target_currency}'
+            with open('currency_converter.txt', 'w') as file:
+                file.write(f'{amount} {base_currency} = {response_json['conversion_result']:.2f} {target_currency}')
         else:
             return 'Error!'
     except requests.exceptions.RequestException as e:
@@ -21,6 +22,6 @@ try:
     base_currency = input('Enter a base currency: ').upper()
     target_currency = input('Enter a target_currency: ').upper()
     amount = int(input('Enter a amount: '))
-    print(currency_converter(base_currency=base_currency, target_currency=target_currency, amount=amount))
+    currency_converter(base_currency=base_currency, target_currency=target_currency, amount=amount)
 except ValueError:
     print('Invalid input. Please enter a numeric value for the amount')
