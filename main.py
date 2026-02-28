9b3f00beb3fff8565e20e230600e7e0d7377465f
Kirill Nadtochaev <Kirillnadtocaev@gmail.com>
2026-02-28 20:09:31 +0300

    New update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-def currency_converter(base_currency, target_currency, amount):
-    api_key = '6d5025f34712b80efd1d73d4'
+
+def currency_converter(api_key, base_currency, target_currency, amount):
     url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{base_currency}/{target_currency}/{amount}'
     try:
         response = requests.get(url)
@@ -18,9 +18,10 @@ def currency_converter(base_currency, target_currency, amount):
         return 'Please enter a valid number for the amount'
 
 try:
+    api_key = input('Enter an API key: ')
     base_currency = input('Enter a base currency: ').upper()
     target_currency = input('Enter a target_currency: ').upper()
     amount = int(input('Enter a amount: '))
-    print(currency_converter(base_currency=base_currency, target_currency=target_currency, amount=amount))
+    print(currency_converter(api_key=api_key, base_currency=base_currency, target_currency=target_currency, amount=amount))
 except ValueError:
     print('Invalid input. Please enter a numeric value for the amount')
